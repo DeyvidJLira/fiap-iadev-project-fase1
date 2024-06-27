33cc058b5aedc4d5923d6e51601dc6ce6d78ebc7
Deyvid Jaguaribe de Lira <deyvidjlira@gmail.com>
2024-06-26 21:06:49 -0300

    feat(process): process data script and maps

---
+++ src/maps.py
@@ -0,0 +1,17 @@
+sexo_map = {
+    "masculino": 0,
+    "feminino": 1
+}
+
+fumante_map = {
+    "nao": 0,
+    "sim": 1
+}
+
+regiao_map = {
+    "norte": 0,
+    "nordeste": 1,
+    "centro oeste": 2,
+    "sul": 3,
+    "sudeste": 4
+}
